217bdf605cf577864951917181103fd82269c56d
ErrorErrorError <16653389+ErrorErrorError@users.noreply.github.com>
2021-11-02 19:54:49 -0500

    Improved pipeline build after update

---
+++ bitbucket-deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Deploy script start (deploy.sh)"
+
+echo ""
+sudo /bin/systemctl restart steds-care.service 
+sudo /bin/systemctl status steds-care.service
+
+echo ""
+sudo /bin/systemctl restart nginx
+sudo /bin/systemctl status nginx
+
+echo "Deploy script finish (deploy.sh)"
