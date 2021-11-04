82e92fc341862b287d1cccb93cecd615976790c4
ErrorErrorError <16653389+ErrorErrorError@users.noreply.github.com>
2021-11-04 14:58:13 -0500

    Added new impl for nginx

--- bitbucket-deploy.sh
+++ bitbucket-deploy.sh
@@ -3,11 +3,11 @@
 echo "Deploy script start (deploy.sh)"
 
 echo ""
-sudo /bin/systemctl restart steds-care.service 
-sudo /bin/systemctl status steds-care.service
+# sudo /bin/systemctl restart steds-care.service 
+# sudo /bin/systemctl status steds-care.service
 
 echo ""
-sudo /bin/systemctl restart nginx
-sudo /bin/systemctl status nginx
+# sudo /bin/systemctl restart nginx
+# sudo /bin/systemctl status nginx
 
 echo "Deploy script finish (deploy.sh)"
--- main.py
+++ main.py
@@ -6,3 +6,22 @@ api = FastAPI()
 @api.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@api.get("/api/v1/organization/login")
+async def organization_login():
+    return { "hahah" : "no" }
+
+
+
+# NGINX Unit Application
+async def application(scope, receive, send):
+
+    await send({
+        'type': 'http.response.start',
+        'status': 200
+    })
+
+    await send({
+        'type': 'http.response.body',
+        'body': b'Hello, ASGI\n'
+    })
