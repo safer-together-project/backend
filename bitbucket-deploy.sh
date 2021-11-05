f47cd91d73ae01ef9c57d3663b0ecdac6a64fffc
ErrorErrorError <16653389+ErrorErrorError@users.noreply.github.com>
2021-11-04 22:01:32 -0500

    Updated pass

--- bitbucket-deploy.sh
+++ bitbucket-deploy.sh
@@ -3,10 +3,10 @@
 echo "Deploy script start (deploy.sh)"
 
 echo "\n\nUpdate Python Dependencies"
+
 cd /home/deploy/steds-care-backend/
 python3 -m venv python-env
-source python-env/bin/activate
-pip install -r requirements.txt
-deactivate
+python-env/bin/pip install -r requirements.txt
+
 
 echo "\nDeploy script finish (deploy.sh)"
