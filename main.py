9051b0d3b90886b2a54adfdf5a02f96af2c326cf
ErrorErrorError <16653389+ErrorErrorError@users.noreply.github.com>
2021-11-04 22:57:49 -0500

    Adding getters and documentation

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
 from fastapi import FastAPI
 
-api = FastAPI()
+description = """
+Steds Care App Api for our research project
+"""
+
+tags_metadata = [
+    {
+        "name": "beacons",
+        "description": "Manages beacons from a given organization."
+    }
+]
+
+api = FastAPI(
+    title="StedsCareApi",
+    description=description,
+    version="0.0.1"
+)
 
 
 @api.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Welcome to Steds Care Api. This is just to test if our api works."}
+ 
+
+# Beacons
+
+@api.get("/api/v1/beacons/", tags=tags_metadata["beacons"])
+async def get_beacons():
+    return { "hahah" : "no" }
+
 
 @api.get("/api/v1/organization/login")
 async def organization_login():
