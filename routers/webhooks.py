16e0dd9bc7098fc5e3aeb1ffe569c8f357e346db
ErrorErrorError <16653389+ErrorErrorError@users.noreply.github.com>
2021-11-12 19:56:09 -0600

    update dir

--- routers/webhooks.py
+++ routers/webhooks.py
@@ -13,7 +13,7 @@ router = APIRouter(
 async def received_payload(request: Request):
     if request.method == 'POST':
         cwd = os.getcwd()
-        subprocess.call(["sh",f"{cwd}/bitbucket-deploy.sh"])
+        subprocess.call(["sh",f"/home/deploy/steds-care-backend/bitbucket-deploy.sh"])
         return 'OK'
     else:
         raise HTTPException(status_code=403, detail="You cannot access this.")
