7c6b3287c819ce93328dfadb20a1dbd26ee86059
ErrorErrorError <16653389+ErrorErrorError@users.noreply.github.com>
2021-11-12 19:43:26 -0600

    run command via shell

--- routers/webhooks.py
+++ routers/webhooks.py
@@ -11,7 +11,7 @@ router = APIRouter(
 @router.post("/", status_code=200)
 async def received_payload(request: Request):
     if request.method == 'POST':
-        subprocess.call(["sh","/home/deploy/steds-care-backend/bitbucket-deploy.sh"])
+        subprocess.call(["sh","/home/deploy/steds-care-backend/bitbucket-deploy.sh"], shell=True)
         return 'OK'
     else:
         raise HTTPException(status_code=403, detail="You cannot access this.")
