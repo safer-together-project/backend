90a1be3d66af6d4351b4aba7eb3ece2b18428294
ErrorErrorError <16653389+ErrorErrorError@users.noreply.github.com>
2021-11-12 23:20:49 -0600

    run script as diff user

--- routers/webhooks.py
+++ routers/webhooks.py
@@ -1,4 +1,5 @@
 import os
+import pwd
 from fastapi import APIRouter
 from fastapi.exceptions import HTTPException
 from fastapi import Request
@@ -13,7 +14,9 @@ router = APIRouter(
 async def received_payload(request: Request):
     if request.method == 'POST':
         cwd = os.getcwd()
-        subprocess.Popen([f"{cwd}/bitbucket-deploy.sh"], user="deploy", shell=True)
+        uid = pwd.getpwnam('deploy')[2]
+        os.setuid(uid)
+        subprocess.Popen([f"{cwd}/bitbucket-deploy.sh"],  shell=True)
         return 'OK'
     else:
         raise HTTPException(status_code=403, detail="You cannot access this.")
