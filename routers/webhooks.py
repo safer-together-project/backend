c39827595da321cbc30a621139fa638cdf9e4547
ErrorErrorError <16653389+ErrorErrorError@users.noreply.github.com>
2021-11-13 00:10:54 -0600

    fix typo

--- routers/webhooks.py
+++ routers/webhooks.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Request
 import httpx
 import asyncio
 
@@ -8,13 +8,13 @@ router = APIRouter(
 )
 
 async def request(client):
-    response = await client.get("http://0.0.0.0:9000/hooks/redeploy-webhook")
+    response = await client.post("http://0.0.0.0:9000/hooks/redeploy-webhook")
     return "ok"
 
 
 @router.post("/", status_code=200)
-async def received_payload():
-    if request.method == 'POST':
+async def received_payload(requests: Request):
+    if requests.method == 'POST':
         async with httpx.AsyncClient() as client:
             tasks = [request(client)]
             result = await asyncio.gather(*tasks)
