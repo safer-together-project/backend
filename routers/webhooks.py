9c5aee69e1566b635b5db9c4ec66671e98b3eeaa
ErrorErrorError <16653389+ErrorErrorError@users.noreply.github.com>
2021-11-12 18:39:24 -0600

    test

--- routers/webhooks.py
+++ routers/webhooks.py
@@ -7,18 +7,9 @@ router = APIRouter(
     responses={404: {"description": "Not found"}}
 )
 
-@router.post("/")
+@router.post("/", status_code=200)
 async def received_payload(
     request: Request,
 ):
-    payload = await request.json()
-
-    default_branch = payload["repository"]
-    if default_branch != None:
-        # script_name = DEPLOY_SCRIPTS[app_name]
-        # # background_tasks.add_task(deploy_application, script_name)
-        return {"message": "Deployment started"}
-
-    else:
-        return {"message": "Unable to process action"}
+    return "Hi" 
 
