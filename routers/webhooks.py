5b472fd217372b10bdd191f771fc9491017ba04f
ErrorErrorError <16653389+ErrorErrorError@users.noreply.github.com>
2021-11-12 19:20:40 -0600

    test run script

--- routers/webhooks.py
+++ routers/webhooks.py
@@ -11,11 +11,11 @@ router = APIRouter(
 @router.post("/", status_code=200)
 async def received_payload(request: Request):
     if request.method == 'POST':
-        data = await request.body()
-        commit_author = data['actor']['username']
-        commit_hash = data['push']['changes'][0]['new']['target']['hash'][:7]
-        commit_url = data['push']['changes'][0]['new']['target']['links']['html']['href']
-        print('Webhook received! %s committed %s' % (commit_author, commit_hash))
+        # data = await request.body()
+        # commit_author = data['actor']['username']
+        # commit_hash = data['push']['changes'][0]['new']['target']['hash'][:7]
+        # commit_url = data['push']['changes'][0]['new']['target']['links']['html']['href']
+        # print('Webhook received! %s committed %s' % (commit_author, commit_hash))
         subprocess.Popen(["/usr/bin/zsh","bitbucket-deploy.sh"])
         return 'OK'
     else:
