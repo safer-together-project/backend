bc84bb1485cb864da532ea571b705bfcf4935e00
ErrorErrorError <16653389+ErrorErrorError@users.noreply.github.com>
2021-11-13 00:09:12 -0600

    foward request to proxy

--- routers/webhooks.py
+++ routers/webhooks.py
@@ -1,18 +1,24 @@
-import os
-from fastapi import APIRouter
-from fastapi.exceptions import HTTPException
-from fastapi import Request
-import subprocess
+from fastapi import APIRouter, HTTPException
+import httpx
+import asyncio
 
 router = APIRouter(
     prefix="/webhooks",
     responses={404: {"description": "Not found"}}
 )
 
+async def request(client):
+    response = await client.get("http://0.0.0.0:9000/hooks/redeploy-webhook")
+    return "ok"
+
+
 @router.post("/", status_code=200)
-async def received_payload(request: Request):
+async def received_payload():
     if request.method == 'POST':
-        return 'OK'
+        async with httpx.AsyncClient() as client:
+            tasks = [request(client)]
+            result = await asyncio.gather(*tasks)
+            return result
     else:
         raise HTTPException(status_code=403, detail="You cannot access this.")
 
