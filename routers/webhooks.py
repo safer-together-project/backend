753313e0c9ef9780bfa6c6f245cfe6473d7921c4
ErrorErrorError <16653389+ErrorErrorError@users.noreply.github.com>
2021-11-12 18:15:06 -0600

    test webhooks

---
+++ routers/webhooks.py
@@ -0,0 +1,25 @@
+from fastapi.params import Header
+from fastapi.routing import APIRoute
+from starlette.requests import Request
+
+
+router = APIRoute(
+    prefix="/webhooks",
+    responses={404: {"description": "Not found"}}
+)
+
+@router.post("/")
+async def received_payload(
+    request: Request,
+):
+    payload = await request.json()
+
+    default_branch = payload["repository"]
+    if default_branch != None:
+        # script_name = DEPLOY_SCRIPTS[app_name]
+        # # background_tasks.add_task(deploy_application, script_name)
+        return {"message": "Deployment started"}
+
+    else:
+        return {"message": "Unable to process action"}
+
