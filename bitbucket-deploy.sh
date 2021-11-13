ad315507e5fd46ca93c92c2674759a3a3740ebe2
ErrorErrorError <16653389+ErrorErrorError@users.noreply.github.com>
2021-11-12 19:17:41 -0600

    update script

--- bitbucket-deploy.sh
+++ bitbucket-deploy.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 echo "Deploy script start (deploy.sh)"
+echo "Fetching latest repo..."
+
+cd /home/deploy/steds-care-backend/
+
+git pull
 
 echo "\n\nUpdate Python Dependencies"
 
-cd /home/deploy/steds-care-backend/
 python3 -m venv python-env
 python-env/bin/pip install -r requirements.txt
 python-env/bin/alembic upgrade head # Migrate our database to the latest migration
-
 
 echo "Reload nginx Unit."
 sudo curl -X GET --unix-socket /var/run/control.unit.sock http://localhost/control/applications/stedscare-api/restart
--- routers/webhooks.py
+++ routers/webhooks.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 from fastapi.exceptions import HTTPException
-from starlette.requests import Request
-
+from fastapi import Request
+import subprocess
 
 router = APIRouter(
     prefix="/webhooks",
@@ -9,17 +9,15 @@ router = APIRouter(
 )
 
 @router.post("/", status_code=200)
-async def received_payload(
-    request: Request,
-):
-
-   if request.method == 'POST':
-      data = await request.json()
-      commit_author = data['actor']['username']
-      commit_hash = data['push']['changes'][0]['new']['target']['hash'][:7]
-      commit_url = data['push']['changes'][0]['new']['target']['links']['html']['href']
-      print('Webhook received! %s committed %s' % (commit_author, commit_hash))
-      return 'OK'
-   else:
-      raise HTTPException(status_code=403, detail="You cannot access this.")
+async def received_payload(request: Request):
+    if request.method == 'POST':
+        data = await request.body()
+        commit_author = data['actor']['username']
+        commit_hash = data['push']['changes'][0]['new']['target']['hash'][:7]
+        commit_url = data['push']['changes'][0]['new']['target']['links']['html']['href']
+        print('Webhook received! %s committed %s' % (commit_author, commit_hash))
+        subprocess.Popen(["/usr/bin/zsh","bitbucket-deploy.sh"])
+        return 'OK'
+    else:
+        raise HTTPException(status_code=403, detail="You cannot access this.")
 
