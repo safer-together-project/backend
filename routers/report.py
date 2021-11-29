073712d188d64e8257a0d3bee77eac67aafcf89d
ErrorErrorError <16653389+ErrorErrorError@users.noreply.github.com>
2021-11-29 03:35:45 -0600

    Update clause

--- routers/report.py
+++ routers/report.py
@@ -20,7 +20,7 @@ router = APIRouter(
 
 @router.get('/{organization_id}', response_model=List[ReportReadWithPathAndPoints])
 async def read_reports(organization_id: str, session: AsyncSession = Depends(get_session)):
-    statement = select(Report).where(Report.organization_id == organization_id)
+    statement = select(Report, Path).where(Report.organization_id == organization_id and Report.id == Path.report_id)
     result = await session.execute(statement)
 
     reports = result.scalars().all()
